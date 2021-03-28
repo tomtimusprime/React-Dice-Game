33297d576689eba45e3358e2a73ed237e9b9af34
Tom Black <tom.black06007@gmail.com>
2021-03-28 08:01:14 -0700

    Added the skeleton of the roll dice component.

--- src/Die.js
+++ src/Die.js
@@ -4,10 +4,7 @@ import './Die.css'
 class Die extends Component {
     render() {
         return (
-            <div>
                 <i className={`Die fas fa-dice-${this.props.face}`}></i>
-                <h2>Testing, Testing</h2>
-            </div>
         )
     }
 }
---
+++ src/RollDie.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class RollDie extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    render() {
+        return;
+    }
+}
