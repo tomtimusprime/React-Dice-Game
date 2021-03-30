2d87b793d7ab4eaf54b42352782d322635f7ded2
Tom Black <tom.black06007@gmail.com>
2021-03-29 21:56:02 -0700

    Added state to RollDie component.

--- src/RollDie.js
+++ src/RollDie.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 class RollDie extends Component {
     constructor(props) {
         super(props);
-        
+        this.state = {
+
+        }
+
     }
     render() {
         return;
