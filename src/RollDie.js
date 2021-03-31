886f6653bb8ff4ac6ad8460b5c1e38d8f6f5de2d
Tom Black <tom.black06007@gmail.com>
2021-03-30 22:10:45 -0700

    Started working on the RollDie component functionality.

--- src/RollDie.js
+++ src/RollDie.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react';
 import Die from "./Die";
 
 class RollDie extends Component {
+    static defaultProps = {
+        sides: ["one", "two", "three", "four", "five", "six"]
+    };
     constructor(props) {
         super(props);
         this.state = {
             die1: "one",
             die2: "one"
         }
-
     }
     render() {
         return (
             <div>
                 <Die face={this.state.die1} />
                 <Die face={this.state.die2} />
+                <button onClick={this.roll}>Roll Dice!</button>
             </div>
         )
     }
