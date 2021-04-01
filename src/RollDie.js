2018b0795271de344dff4f384f82f9d59042da7d
Tom Black <tom.black06007@gmail.com>
2021-04-01 12:53:57 -0700

    Added animation to the dice game.

--- src/RollDie.js
+++ src/RollDie.js
@@ -9,7 +9,8 @@ class RollDie extends Component {
         super(props);
         this.state = {
             die1: "one",
-            die2: "one"
+            die2: "one",
+            rolling: false
         }
         this.roll = this.roll.bind(this);
     }
@@ -18,16 +19,23 @@ class RollDie extends Component {
         //pick 2 rolls
         const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
         const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
-        this.setState({ die1: newDie1, die2: newDie2 });
+        this.setState({ die1: newDie1, die2: newDie2, rolling: true });
+
+        //wait 1 second and then reset rolling variable to false
+        setInterval(() => {
+            this.setState({rolling: false})
+        }, 2000);
     }
     render() {
         return (
             <div className="RollDie">
                 <div className="RollDie-Container">
-                    <Die face={this.state.die1} />
-                    <Die face={this.state.die2} />
+                    <Die face={this.state.die1} rolling={this.state.rolling}/>
+                    <Die face={this.state.die2} rolling={this.state.rolling}/>
                 </div>
-                <button onClick={this.roll}>Roll Dice!</button>
+                <button onClick={this.roll} disabled={this.state.rolling}>
+                {this.state.rolling ? 'Rolling...' : 'Roll the dice!'}
+                </button>
             </div>
         )
     }
