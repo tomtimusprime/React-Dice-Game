d1b246b175ce5b5272589e58028f5afbb2a77502
Tom Black <tom.black06007@gmail.com>
2021-03-31 20:25:28 -0700

    Added RollDie functionality and implemented some styling on it as well.

--- src/RollDie.js
+++ src/RollDie.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Die from "./Die";
-
+import "./RollDie.css";
 class RollDie extends Component {
     static defaultProps = {
         sides: ["one", "two", "three", "four", "five", "six"]
@@ -11,12 +11,22 @@ class RollDie extends Component {
             die1: "one",
             die2: "one"
         }
+        this.roll = this.roll.bind(this);
+    }
+
+    roll() {
+        //pick 2 rolls
+        const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+        const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+        this.setState({ die1: newDie1, die2: newDie2 });
     }
     render() {
         return (
-            <div>
-                <Die face={this.state.die1} />
-                <Die face={this.state.die2} />
+            <div className="RollDie">
+                <div className="RollDie-Container">
+                    <Die face={this.state.die1} />
+                    <Die face={this.state.die2} />
+                </div>
                 <button onClick={this.roll}>Roll Dice!</button>
             </div>
         )
